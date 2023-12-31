3f30d4d0c48e1c8ef915fe228ad41091831a030a
Piers <piersmoly@gmail.com>
2023-12-31 12:08:05 +0000

    Create ProjectTrackerApp

---
+++ ProjectTracker/ProjectTracker/ProjectTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectTrackerApp.swift
+//  ProjectTracker
+//
+//  Created by Molyneux, Piers on 31/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ProjectTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
